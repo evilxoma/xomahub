098cb9b2950cf7da5d995ac001f053f83cdd8866
evilxoma <dima9102011@gmail.com>
2026-08-10 04:41:27 +0500

    Pin V33 bootstrap to immutable commit

--- ctdui_v33.lua
+++ ctdui_v33.lua
@@ -1,8 +1,9 @@
 -- XOMA / CTDIG cache-busted bootstrap
 -- Build: PASS23-NONBLOCKING-SKIP-V33
 
+-- Immutable commit URL: this cannot resolve to a stale older ctdui build.
 local source = game:HttpGet(
-    "https://raw.githubusercontent.com/evilxoma/xomahub/refs/heads/main/ctdui.lua?build=PASS23-NONBLOCKING-SKIP-V33"
+    "https://raw.githubusercontent.com/evilxoma/xomahub/be5bdb4c68e59796ecba8bad836cbea945b77c02/ctdui.lua"
 )
 local chunk, err = loadstring(source)
 if not chunk then
